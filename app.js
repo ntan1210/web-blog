429ca01578c0397c9aaf3c7632d911f02967d17e
An Nguyen <ntan1210@gmail.com>
2022-03-01 07:56:22 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send("Good job An! You made it");
+});
+
+app.listen(port, () => console.log(`The App is listening on port ${port}!`));
